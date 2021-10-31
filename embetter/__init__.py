2d00115f6f8a11c7c11e1d204c2480b90e446ad2
Vincent <vincentwarmerdam@gmail.com>
2021-10-31 17:16:06 +0100

    class

--- embetter/__init__.py
+++ embetter/__init__.py
@@ -1,1 +1,8 @@
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
+
 __version__ = '0.0.1'
+
+
+class Embetter(BaseEstimator, TransformerMixin, ClassifierMixin):
+    def __init__(self):
+        pass
