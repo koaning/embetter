59ebaf6b7c4d701c03339e9805847f50d86e902b
Vincent <vincentwarmerdam@gmail.com>
2022-07-16 17:42:21 +0200

    columngrabber

--- embetter/grab.py
+++ embetter/grab.py
@@ -15,3 +15,21 @@ class ImageGrabber(EmbetterBase):
         Turn a file path into numpy array containing pixel values.
         """
         return [Image.open(x).convert(self.convert) for x in X]
+
+
+class ColumnGrabber(EmbetterBase):
+    """
+    Component that can grab a pandas column as a list.
+
+    This can be useful when dealing with text encoders as these
+    sometimes cannot deal with pandas columns.
+    """
+
+    def __init__(self, colname) -> None:
+        self.colname = colname
+
+    def transform(self, X, y=None):
+        """
+        Takes a column from pandas and returns it as a list.
+        """
+        return [x for x in X[self.colname]]
