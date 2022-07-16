d0e1877af071a221fc70868f3734f15f48d34860
Vincent <vincentwarmerdam@gmail.com>
2022-07-16 17:37:05 +0200

    black

--- embetter/grab.py
+++ embetter/grab.py
@@ -1,4 +1,4 @@
-from PIL import Image 
+from PIL import Image
 from embetter.base import EmbetterBase
 
 
@@ -6,9 +6,10 @@ class ImageGrabber(EmbetterBase):
     """
     Component that can turn image paths into numpy arrays.
     """
-    def __init__(self, convert='RGB') -> None:
+
+    def __init__(self, convert="RGB") -> None:
         self.convert = convert
-    
+
     def transform(self, X, y=None):
         """
         Turn a file path into numpy array containing pixel values.
--- embetter/image.py
+++ embetter/image.py
@@ -1,4 +1,4 @@
-import torch 
+import torch
 import numpy as np
 from embetter.base import EmbetterBase
 
@@ -9,10 +9,28 @@ class TorchVision(EmbetterBase):
 
     You can find a list of available models here:
     https://pytorch.org/vision/stable/models.html#table-of-all-available-classification-weights
+
+    Usage:
+
+    ```python
+    from embetter import TorchVision
+
+    # MobileNet
+    TorchVision("pytorch/vision", "mobilenet_v2", "MobileNet_V2_Weights.IMAGENET1K_V2")
+    ```
     """
-    def __init__(self, repo_or_dir="pytorch/vision", model="resnet50", weights_name="ResNet50_Weights.IMAGENET1K_V2"):
+
+    def __init__(
+        self,
+        repo_or_dir="pytorch/vision",
+        model_name="resnet50",
+        weights_name="ResNet50_Weights.IMAGENET1K_V2",
+    ):
+        self.repo_or_dir = repo_or_dir
+        self.weights_name = weights_name
+        self.model_name = model_name
         self.weights = torch.hub.load(repo_or_dir, "get_weight", name=weights_name)
-        self.model = torch.hub.load(repo_or_dir, model, weights=self.weights)
+        self.model = torch.hub.load(repo_or_dir, model_name, weights=self.weights)
         self.preprocess = self.weights.transforms()
 
     def transform(self, X, y=None):
