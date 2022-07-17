94e6e1720233a2a136d848710fa10b0990e3297c
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-07-17 08:39:14 +0000

    added-1st-version

--- embetter/vision/_colorhist.py
+++ embetter/vision/_colorhist.py
@@ -1,16 +1,22 @@
 import numpy as np
-import skimage
-import matplotlib.pyplot as plt
- 
-image = skimage.data.chelsea()
-image_red, image_green, image_blue = image[:,:,0], image[:,:,1], image[:,:,2]
- 
-fig, ax = plt.subplots(2,3)
-ax[0,0].imshow(image_red, cmap='gray')
-ax[0,1].imshow(image_green, cmap='gray')
-ax[0,2].imshow(image_blue, cmap='gray')
- 
-bins = np.arange(-0.5, 255+1,1)
-ax[1,0].hist(image_red.flatten(), bins = bins, color='r')
-ax[1,1].hist(image_green.flatten(), bins=bins, color='g')
-ax[1,2].hist(image_blue.flatten(), bins=bins, color='b')
+from embetter.base import EmbetterBase
+
+
+class ColorHistogram(EmbetterBase):
+    """
+    Arguments:
+    - n_buckets: number of buckets per color
+    """
+    def __init__(self, n_buckets=256):
+        self.n_buckets = n_buckets
+    
+    def transform(self, X, y=None):
+        output = np.zeros((len(X), self.n_buckets * 3))
+        for i, x in enumerate(X):
+            arr = np.array(x)
+            output[i,:] = np.concatenate([
+                np.histogram(arr[:,:,0].flatten(), bins=np.linspace(0, 255, self.n_buckets))[0],
+                np.histogram(arr[:,:,1].flatten(), bins=np.linspace(0, 255, self.n_buckets))[0],
+                np.histogram(arr[:,:,2].flatten(), bins=np.linspace(0, 255, self.n_buckets))[0],
+            ])
+        return output
