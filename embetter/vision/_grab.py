06f9609cfe09556103db64fb635499fc6adc1b6b
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-09-15 17:21:24 +0000

    timm

--- embetter/vision/_grab.py
+++ embetter/vision/_grab.py
@@ -1,5 +1,6 @@
-from embetter.base import EmbetterBase
+import numpy as np
 from PIL import Image
+from embetter.base import EmbetterBase
 
 
 class ImageGrabber(EmbetterBase):
@@ -7,11 +8,23 @@ class ImageGrabber(EmbetterBase):
     Component that can turn image paths into numpy arrays.
     """
 
-    def __init__(self, convert="RGB") -> None:
+    def __init__(self, convert="RGB", out="PIL") -> None:
         self.convert = convert
+        self.out = out
+
+    def fit(self, X, y=None):
+        """
+        Checks if input params are good
+        """
+        if self.out not in ["pil", "numpy"]:
+            raise ValueError(f"Output format parameter out={self.out} must be either pil/numpy.")
+        return self
 
     def transform(self, X, y=None):
         """
         Turn a file path into numpy array containing pixel values.
         """
-        return [Image.open(x).convert(self.convert) for x in X]
+        if self.out == "PIL":
+            return [Image.open(x).convert(self.convert) for x in X]
+        if self.out == "numpy":
+            return np.array([np.array(Image.open(x).convert(self.convert)) for x in X])
--- tests/data/test_base.py
+++ tests/data/test_base.py
@@ -5,6 +5,6 @@ from embetter.grab import ColumnGrabber
 def test_grab_column():
     """Ensure that we can grab a text column."""
     data = [{"text": "hi", "foo": 1}, {"text": "yes", "foo": 2}]
-    df = pd.DataFrame(data)
-    out = ColumnGrabber("text").fit_transform(df)
+    dataframe = pd.DataFrame(data)
+    out = ColumnGrabber("text").fit_transform(dataframe)
     assert out == ["hi", "yes"]
