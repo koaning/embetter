218fd14517dd09af50d70b08762f7f6ccc94494f
Carlo Lepelaars <info@carlolepelaars.nl>
2022-11-09 16:26:04 +0100

    Option for verbose progress bar in `test_text`

--- tests/test_text.py
+++ tests/test_text.py
@@ -4,7 +4,7 @@ from embetter.text import SentenceEncoder
 
 
 def test_basic_sentence_encoder():
-    encoder = SentenceEncoder()
+    encoder = SentenceEncoder(show_progress_bar=True)
     output_dim = encoder.tfm._modules['1'].word_embedding_dimension
     test_sentences = ["This is a test sentence!", "And this is another one", "\rUnicode stuff: ♣️,♦️,❤️,♠️\n"]
     output = encoder.fit_transform(test_sentences)
