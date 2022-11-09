5716e87f64bf9c370708965b1da48ca6162cbec5
Carlo Lepelaars <info@carlolepelaars.nl>
2022-11-09 16:11:23 +0100

    Dynamic `SentenceEncoder` tests

--- tests/test_text.py
+++ tests/test_text.py
@@ -5,8 +5,9 @@ from embetter.text import SentenceEncoder
 
 def test_basic_sentence_encoder():
     encoder = SentenceEncoder()
+    output_dim = encoder.tfm._modules['1'].word_embedding_dimension
     test_sentences = ["This is a test sentence!", "And this is another one", "\rUnicode stuff: ♣️,♦️,❤️,♠️\n"]
     output = encoder.fit_transform(test_sentences)
     assert isinstance(output, np.ndarray)
-    assert output.shape == (len(test_sentences), 384)
+    assert output.shape == (len(test_sentences), output_dim)
 
