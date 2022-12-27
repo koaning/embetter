f1f38463168d2a0d1b9d9a965c3d70ea2e6f7ddf
Vincent <vincentwarmerdam@gmail.com>
2022-12-27 16:32:13 +0100

    end of day

--- embetter/finetune/_forward.py
+++ embetter/finetune/_forward.py
@@ -15,36 +15,41 @@ class FeedForwardModel(nn.Module):
         return self.sigmoid(self.linear(self.embed(x)))
     
     def embed(self, x):
-        return self.relu(self.hidden(x))
+        return self.hidden(x)
 
 
 class ForwardFinetuner(BaseEstimator, TransformerMixin):
-    def __init__(self, hidden_dim=50, n_epochs=10, learning_rate=0.001) -> None:
+    def __init__(self, hidden_dim=50, n_epochs=500, learning_rate=0.01) -> None:
         self.hidden_dim = hidden_dim
         self.n_epochs = n_epochs
         self.learning_rate = learning_rate
 
     def fit(self, X, y):
-        for _ in range(self.n_epochs):
-            self.partial_fit(X, y, classes=np.unique(y))
-        return self
+        return self.partial_fit(X, y, classes=np.unique(y))
     
     def partial_fit(self, X, y, classes=None):
-        if not self._classes:
+        if not hasattr(self, "_classes"):
+            if classes is None:
+                raise ValueError("`classes` must be provided for partial_fit")
             self._classes = classes
         # Create a model if it does not exist yet. 
-        if not self._model:
+        if not hasattr(self, "_model"):
             self._model = FeedForwardModel(X.shape[1], self.hidden_dim, len(self._classes))
-            self._optimizer = torch.optim.SGD(self._model.parameters(), lr=self.learning_rate)
+            self._optimizer = torch.optim.Adam(self._model.parameters(), lr=self.learning_rate)
             self._criterion = nn.CrossEntropyLoss()
-
-        self._optimizer.zero_grad()
-        out = self._model(X)
-        loss = self._criterion(out, y)
-        loss.backward()
-        self._optimizer.step()
+        
+        torch_X = torch.from_numpy(X).detach().float()
+        torch_y = torch.from_numpy(y).detach()
+        
+        for _ in range(self.n_epochs):
+            self._optimizer.zero_grad()
+            out = self._model(torch_X)
+            loss = self._criterion(out, torch_y)
+            loss.backward()
+            self._optimizer.step()
 
         return self
 
     def transform(self, X, y=None):
-        return self._model.embed(X).detach().numpy()
+        Xt = torch.from_numpy(X).float().detach()
+        return self._model.embed(Xt).detach().numpy()
