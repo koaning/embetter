31d44a07c67a25fa98264581c4d74d8c48026329
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-03-31 16:48:53 +0200

    Update _forward.py

--- embetter/finetune/_forward.py
+++ embetter/finetune/_forward.py
@@ -12,7 +12,6 @@ class FeedForwardModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedForwardModel, self).__init__()
         self.hidden = nn.Linear(input_dim, hidden_dim)
-        self.relu = nn.ReLU()
         self.linear = nn.Linear(hidden_dim, output_dim)
         self.sigmoid = nn.Sigmoid()
 
