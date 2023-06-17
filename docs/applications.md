b95940e96f909374a0cdd8c3fc860434e44b5dd7
Vincent <vincentwarmerdam@gmail.com>
2023-06-17 14:25:49 +0200

    nice

--- docs/applications.md
+++ docs/applications.md
@@ -5,15 +5,54 @@ title: Applications
 This document contains some tricks, hints and demos of applications that you might want to consider
 in combination with this library. 
 
+## Cache
+
+Calculating embeddings can be expensive, even costly when you're using external providers. 
+This is why this library offers an integration with [diskcache](https://grantjenks.com/docs/diskcache/). 
+That way, you can infer the embeddings once and store them to disk for later.
+
+Here's an example of how you might run that. 
+
+```python
+from embetter.text import SentenceEncoder
+from embetter.utils import cached
+
+encoder = cached("sentence-enc", SentenceEncoder('all-MiniLM-L6-v2'))
+
+examples = [f"this is a pretty long text, which is more expensive {i}" for i in range(10_000)]
+
+# This might be a bit slow ~17.2s on our machine
+encoder.transform(examples)
+
+# This should be quicker ~4.71s on our machine
+encoder.transform(examples)
+```
+
+Note that you're also able to fetch the precalculated embeddings directly via: 
+
+```python
+from diskcache import Cache
+
+# Make sure that you use the same name as in `cached`
+cache = Cache("sentence-enc")
+# Use a string as a key, if it's precalculated you'll get an array back.
+cache["this is a pretty long text, which is more expensive 0"]
+```
+
+Be mindful of what does in to the encoder that you choose. It's preferable to give it
+text as opposed to numpy arrays. Also note that the first time that you'll run this
+it will take more time due to the overhead of writing into the cache.
+
 ## Speedup with Modal 
 
 Embedding text can be slow, especially when you're running on a CPU. If you wish 
 to speed up your embedding calculations you may enjoy using [modal](https://modal.com/). 
 Modal allows you to add a GPU to a Python function simply by adding a decorator.
 
-Not every encoder in embetter will get a speedup by using a GPU but the
-`SentenceEncoder` as well as `ClipEncoder` should both automatically detect
-when the GPU is available automatically.
+Not every encoder in embetter will get a speedup by using a GPU. But we've done some 
+benchmarks and noticed that
+`SentenceEncoder` as well as `ClipEncoder` should both benefit. These components will
+also automatically detect when the GPU is available automatically.
 
 The code below gives an example. 
 
---
+++ embetter/utils.py
@@ -0,0 +1,66 @@
+import numpy as np 
+from typing import Callable 
+from diskcache import Cache
+from sklearn.base import BaseEstimator
+
+def cached(name: str, pipeline: BaseEstimator):
+    """
+    Uses a [diskcache](https://grantjenks.com/docs/diskcache/tutorial.html) in
+    an attempt to fetch precalculated embeddings from disk instead of inferring them.
+    This can save on compute, but also cloud credits, depending on the backend
+    that you're using to generate embeddings.
+
+    Be mindful of what does in to the encoder that you choose. It's preferable to give it
+    text as opposed to numpy arrays. Also note that the first time that you'll run this
+    it will take more time due to the overhead of writing into the cache.
+
+    Arguments:
+        name: the name of the local folder to represent the disk cache
+        pipeline: the pipeline that you want to cache 
+
+    Usage:
+    ```python
+    from embetter.text import SentenceEncoder
+    from embetter.utils import cached
+
+    encoder = cached("sentence-enc", SentenceEncoder('all-MiniLM-L6-v2'))
+
+    examples = [f"this is a pretty long text, which is more expensive {i}" for i in range(10_000)]
+    
+    # This might be a bit slow ~17.2s on our machine
+    encoder.transform(examples)
+
+    # This should be quicker ~4.71s on our machine
+    encoder.transform(examples)
+    ```
+
+    Note that you're also able to fetch the precalculated embeddings directly via: 
+
+    ```python
+    from diskcache import Cache
+
+    # Make sure that you use the same name as in `cached`
+    cache = Cache("sentence-enc")
+    # Use a string as a key, if it's precalculated you'll get an array back.
+    cache["this is a pretty long text, which is more expensive 0"]
+    ```
+    """
+    cache  = Cache(name)
+
+    def run_cached(method: Callable):
+        def wrapped(X, y=None):
+            results = {i: cache[x] if x in cache else "TODO" for i, x in enumerate(X)}
+            text_todo = [X[i] for i, x in results.items() if x == "TODO"]
+            i_todo = [i for i, x in results.items() if x == "TODO"]
+            out = method(text_todo)
+            with Cache(cache.directory) as reference:
+                for i, text, x_tfm in zip(i_todo, text_todo, out):
+                    results[i] = x_tfm
+                    cache.set(text, x_tfm)
+            assert all([v != "TODO" for v in results.values()])
+            return np.array([arr for i, arr in results.items()])
+        return wrapped
+    
+    pipeline.transform = run_cached(pipeline.transform)
+    
+    return pipeline
