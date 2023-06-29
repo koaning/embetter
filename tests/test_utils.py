0b93b81b2b9b1a9864e03721d9ec61fc2520a003
Vincent <vincentwarmerdam@gmail.com>
2023-06-29 17:35:37 +0200

    style

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,9 +5,16 @@ from embetter.text import SentenceEncoder
 
 
 def test_calc_distances():
-    text_in = ['hi there', 'no', 'what is this then']
-    
-    dists1 = calc_distances(text_in, ['greetings', "something else"], SentenceEncoder(), aggregate=np.min)
-    dists2 = calc_distances(text_in, ['greetings', "something unrelated"], SentenceEncoder(), aggregate=np.min)
-    assert np.isclose(dists1.min(), dists2.min())
+    """Make sure that the aggregation works as expected"""
+    text_in = ["hi there", "no", "what is this then"]
 
+    dists1 = calc_distances(
+        text_in, ["greetings", "something else"], SentenceEncoder(), aggregate=np.min
+    )
+    dists2 = calc_distances(
+        text_in,
+        ["greetings", "something unrelated"],
+        SentenceEncoder(),
+        aggregate=np.min,
+    )
+    assert np.isclose(dists1.min(), dists2.min())
