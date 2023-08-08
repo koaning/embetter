634e6fce014ea31337522a23179d3101e44e7984
Márton Kardos <power.up1163@gmail.com>
2023-08-08 08:34:31 +0200

    Added test for Word2Vec

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,18 +1,39 @@
-import pytest
 import numpy as np
-
-from spacy.vocab import Vocab
+import pytest
+from gensim.models import Word2Vec
+from gensim.utils import tokenize
 from spacy.language import Language
+from spacy.vocab import Vocab
 
-from embetter.text import SentenceEncoder, BytePairEncoder, spaCyEncoder
+from embetter.text import (
+    BytePairEncoder,
+    SentenceEncoder,
+    Word2VecEncoder,
+    spaCyEncoder,
+)
 from embetter.utils import cached
-
 
 test_sentences = [
     "This is a test sentence!",
     "And this is another one",
     "\rUnicode stuff: ♣️,♦️,❤️,♠️\n",
 ]
+
+
+@pytest.mark.parametrize("setting", ["max", "mean", "both"])
+def test_word2vec(setting):
+    """Check if one can train and use a very simple word embedding model."""
+    vector_size = 25
+    sentences = [list(tokenize(sent)) for sent in test_sentences]
+    model = Word2Vec(
+        sentences=sentences, vector_size=vector_size, window=3, min_count=1
+    )
+    encoder = Word2VecEncoder(model, agg=setting)
+    output = encoder.fit_transform(test_sentences)
+    assert isinstance(output, np.ndarray)
+    out_dim = vector_size if setting != "both" else vector_size * 2
+    assert output.shape == (len(test_sentences), out_dim)
+    assert repr(encoder)
 
 
 def test_basic_sentence_encoder():
