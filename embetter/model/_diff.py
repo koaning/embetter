7ee89b429c0028ca0eed8eb50c0f3421c9f7a72b
Vincent <vincentwarmerdam@gmail.com>
2023-08-31 15:44:27 +0200

    less trict on style

--- embetter/model/_diff.py
+++ embetter/model/_diff.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.base import TransformerMixin, ClassifierMixin
 
@@ -5,15 +7,15 @@ from sklearn.base import TransformerMixin, ClassifierMixin
 class DifferenceClassifier:
     """
     Classifier for similarity using encoders under the hood.
-    
+
     It's similar to the scikit-learn models that you're used to, but it accepts
     two inputs `X1` and `X2` and tries to predict if they are similar. Effectively
-    it's just a classifier on top of `diff(X1 - X2)`. 
+    it's just a classifier on top of `diff(X1 - X2)`.
 
     Arguments:
-        enc: scikit-learn compatbile encoder of the input data 
+        enc: scikit-learn compatbile encoder of the input data
         clf_head: the classifier to apply at the end
-    
+
     Usage:
 
     ```python
@@ -34,9 +36,12 @@ class DifferenceClassifier:
     mod.predict(X1=texts1, X2=texts2)
     ```
     """
-    def __init__(self, enc: TransformerMixin, clf_head:ClassifierMixin=None):
+
+    def __init__(self, enc: TransformerMixin, clf_head: ClassifierMixin = None):
         self.enc = enc
-        self.clf_head = LogisticRegression(class_weight="balanced") if not clf_head else clf_head
+        self.clf_head = (
+            LogisticRegression(class_weight="balanced") if not clf_head else clf_head
+        )
 
     def _calc_feats(self, X1, X2):
         return np.abs(self.enc(X1) - self.enc(X2))
