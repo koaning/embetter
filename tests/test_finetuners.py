aec224855bdddfc8c51aeeaaa14596111d826095
Vincent <vincent@calmcode.io>
2023-10-28 21:21:01 +0200

    add test

--- tests/test_finetuners.py
+++ tests/test_finetuners.py
@@ -1,5 +1,5 @@
 import pytest
-from embetter.finetune import FeedForwardTuner, ContrastiveTuner
+from embetter.finetune import FeedForwardTuner, ContrastiveTuner, ContrastiveLearner
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -16,3 +16,15 @@ def test_finetuner_basics(finetuner, hidden_dim):
     fft = finetuner(hidden_dim=hidden_dim)
     fft.fit(X_tfm.todense(), labels)
     assert fft.transform(X_tfm.todense()).shape[1] == hidden_dim
+
+
+@pytest.mark.parametrize("learner", [ContrastiveLearner])
+@pytest.mark.parametrize("shape_out", [25, 50, 75])
+def test_learner_basics(learner, shape_out):
+    """https://github.com/koaning/embetter/issues/38"""
+    cv = CountVectorizer()
+    X_tfm = cv.fit(texts).transform(texts)
+    fft = learner(shape_out=shape_out)
+    X1, X2 = X_tfm.todense(), X_tfm.todense()
+    fft.fit(X1, X2, [0, 1, 0])
+    assert fft.transform(X_tfm.todense()).shape[1] == shape_out
