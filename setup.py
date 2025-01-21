021f1bb24e977f74536229ac8486de5bfad31f5b
vincent d warmerdam <vincentwarmerdam@gmail.com>
2025-01-21 12:44:41 +0000

    bump

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ dev_packages = all_packages + docs_packages + test_packages
 
 setup(
     name="embetter",
-    version="0.6.5",
+    version="0.6.6",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks", "docs", "datasets"]),
     description="Just a bunch of useful embeddings to get started quickly.",
