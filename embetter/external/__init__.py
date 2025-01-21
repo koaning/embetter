9d386eeb39770250854b2e396c2031ae559c34da
Max Scheijen <maxscheijen@gmail.com>
2025-01-21 12:53:59 +0100

    seperate AzureOpenAIEncoder NotInstalled error handling

--- embetter/external/__init__.py
+++ embetter/external/__init__.py
@@ -1,9 +1,13 @@
 from embetter.error import NotInstalled
 
 try:
-    from ._openai import AzureOpenAIEncoder, OpenAIEncoder
+    from ._openai import OpenAIEncoder
 except ModuleNotFoundError:
     OpenAIEncoder = NotInstalled("OpenAIEncoder", "openai")
+
+try:
+    from ._openai import AzureOpenAIEncoder
+except ModuleNotFoundError:
     AzureOpenAIEncoder = NotInstalled("AzureOpenAIEncoder", "openai")
 
 try:
